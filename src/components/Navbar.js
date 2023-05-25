f590878cddd0357384ac2ef36efaf80721a3a278
Developer <balsadas>
2023-05-25 11:08:18 +0500

    regtrsfgv

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,49 +4,7 @@ import Category from './Category';
 import Search from './Search';
 
 function Navbar() {
-  // const [users, setUsers] = useState(false)
-
-
-
-  // const addPost = data => {
-
-
-  // const headers = new Headers()
-  // //headers.append('Content-type', 'application/json;charset-utf8')
-  // headers.append('Authorization', 'efjklerlfkdr')
-
-
-  // const formData= new FormData()
-  // formData.append('userId',  data.userId)
-  // formData.append('title', data.title)
-  // formData.append('body', data.body)
-
-  //   fetch('https://jsonplaceholder.typicode.com/posts', {
-  //     method: 'Post',
-  //     body: formData,
-  //     headers
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  //     .catch(err => console.log(err))
-  // }
-
-  // useEffect(() => {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(res => {
-  //       if (res.ok && res.status === 200) {
-  //         return res.json()
-  //       }
-  //     })
-  //     .then(data => setUsers(data))
-  //     .catch(err => console.log(err))
-
-  //   addPost({
-  //     userId: 1,
-  //     title: 'new Project',
-  //     body: 'psot'
-  //   })
-  // }, [])
+ 
   const kat = [
     { id: 1, text: "Şahyrlar" },
     { id: 2, text: 'Poema' },
