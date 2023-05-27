7a4fddaa18cdceea0e916b1cc132a46af6289ead
Developer <balsadas>
2023-05-27 09:55:59 +0500

    dfddf

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,9 +13,7 @@ console.log(cookies.get('token'))
 function Nav() {
   const [click, setClick] = useState(0)
   const [dclick, setDclick] = useState(false)
-  const clicked = ()=>{
-    console.log('asdsadadsa')
-    }
+
 
   var showdate = new Date();
   var displaytodaysdate = `${showdate.getDate()}.${showdate.getMonth() + 1}.${showdate.getFullYear()}`;
@@ -61,7 +59,7 @@ function Nav() {
               <div className='border mt-1 mb-1'></div>
               <div  className='flex items-center cursor-pointer  lg:mt-2 pl-3 p-2 lg:mb-3 md:mb-1 hover:bg-slate-300 w-full'>
                   <img src='./image/logout.png' className='md:w-[1vw] md:h-[1vw] w-[3vw] h-[3vw]'/>
-                  <p onClick={clicked()} className='ml-4 md:text-[1vw] text-[3vw]' >Çyk</p>
+                  <p  className='ml-4 md:text-[1vw] text-[3vw]' >Çyk</p>
               </div>
           </div>
       </div>
