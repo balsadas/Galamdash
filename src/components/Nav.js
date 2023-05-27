beda8248c32d42fd2f0311a8a33130c859d70777
()gres <serdarsaryjayev2002@gmail.com>
2023-05-27 09:48:33 +0500

    Change edit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,12 +10,12 @@ import Cookies from 'universal-cookie';
 
 const cookies = new Cookies()
 console.log(cookies.get('token'))
-function Nav() {
+function Nav({ change, setChange }) {
   const [click, setClick] = useState(0)
   const [dclick, setDclick] = useState(false)
-  const clicked = ()=>{
+  const clicked = () => {
     console.log('asdsadadsa')
-    }
+  }
 
   var showdate = new Date();
   var displaytodaysdate = `${showdate.getDate()}.${showdate.getMonth() + 1}.${showdate.getFullYear()}`;
@@ -31,15 +31,15 @@ function Nav() {
           <div className='flex '>
 
             {!cookies.get('token') && <Link to='/Register'>
-              <div  className='flex select-none items-center group md:mr-2 pr-3  cursor-pointer hover:-translate-y-1 hover:scale-110 transition-all duration-500 ease-in'>
+              <div className='flex select-none items-center group md:mr-2 pr-3  cursor-pointer hover:-translate-y-1 hover:scale-110 transition-all duration-500 ease-in'>
                 <img src='./image/user1.png' className='md:w-[1.8vw] md:h-[1.8vw] w-[4vw] h-[4vw]' />
                 <h4 className='group-hover:block hidden md:text-[1.1vw] text-[3vw] ml-2 font-bold'>Içeri gir</h4>
               </div>
             </Link>}
             {cookies.get('token') &&
-            <div onClick={() => setDclick(!dclick)} className='flex select-none items-center group md:mr-2 pr-3  cursor-pointer hover:-translate-y-1 hover:scale-110 transition-all duration-500 ease-in'>
-              <img src={ `${setting.SERVER}/${cookies.get('image')}`  } className='md:w-[1.8vw] md:h-[1.8vw] w-[4vw] h-[4vw]'/>
-            </div>}
+              <div onClick={() => setDclick(!dclick)} className='flex select-none items-center group md:mr-2 pr-3  cursor-pointer hover:-translate-y-1 hover:scale-110 transition-all duration-500 ease-in'>
+                <img src={`${setting.SERVER}/${cookies.get('image')}`} className='md:w-[1.8vw] md:h-[1.8vw] w-[4vw] h-[4vw]' />
+              </div>}
           </div>
         </div>
 
@@ -47,25 +47,25 @@ function Nav() {
 
 
         {/* dropdown registrasiya bolanda ulanmaly */}
-          <div className={dclick ? 'block z-[999]' : 'hidden'}>
+        <div className={dclick ? 'block z-[999]' : 'hidden'}>
           <div className=' flex justify-end select-none '>
-          <div className=' bg-white md:w-[11vw] w-[35vw] absolute  shadow-lg rounded-lg'>
+            <div className=' bg-white md:w-[11vw] w-[35vw] absolute  shadow-lg rounded-lg'>
               <div className='flex items-center pl-3 p-2 lg:mt-2 cursor-pointer hover:bg-slate-300 w-full'>
-                  <img src='./image/userd.png' className='md:w-[1vw] md:h-[1vw] w-[3vw] h-[3vw]' />
-                  <p className='ml-2 md:text-[1vw] text-[3vw] '>{cookies.get('nick')}</p>
+                <img src='./image/userd.png' className='md:w-[1vw] md:h-[1vw] w-[3vw] h-[3vw]' />
+                <p className='ml-2 md:text-[1vw] text-[3vw] '>{cookies.get('nick')}</p>
               </div>
               <div className='flex items-center lg:mt-2 p-2 cursor-pointer hover:bg-slate-300 w-full'>
-                  <img src='./image/key.png' className='md:w-[1vw] md:h-[1vw] w-[3vw] h-[3vw]' />
-                  <p className='ml-2 md:text-[1vw] text-[3vw] '>Paroly üýtget</p>
+                <img src='./image/key.png' className='md:w-[1vw] md:h-[1vw] w-[3vw] h-[3vw]' />
+                <p className='ml-2 md:text-[1vw] text-[3vw] '>Paroly üýtget</p>
               </div>
               <div className='border mt-1 mb-1'></div>
-              <div  className='flex items-center cursor-pointer  lg:mt-2 pl-3 p-2 lg:mb-3 md:mb-1 hover:bg-slate-300 w-full'>
-                  <img src='./image/logout.png' className='md:w-[1vw] md:h-[1vw] w-[3vw] h-[3vw]'/>
-                  <p onClick={clicked()} className='ml-4 md:text-[1vw] text-[3vw]' >Çyk</p>
+              <div className='flex items-center cursor-pointer  lg:mt-2 pl-3 p-2 lg:mb-3 md:mb-1 hover:bg-slate-300 w-full'>
+                <img src='./image/logout.png' className='md:w-[1vw] md:h-[1vw] w-[3vw] h-[3vw]' />
+                <p onClick={clicked()} className='ml-4 md:text-[1vw] text-[3vw]' >Çyk</p>
               </div>
+            </div>
           </div>
-      </div>
-  
+
         </div>
 
 
